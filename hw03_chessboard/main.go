179f36e233157f37f9895af240f27c501b51c0a2
PapaDjo2000 <maksimdadychyn@gmail.com>
2024-10-25 19:12:49 +0300

    v1.0

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size_x int
+	var size_y int
+
+	fmt.Println("Введите Х")
+	fmt.Scanf("%d", &size_x)
+
+	fmt.Println("Введите У")
+	fmt.Scanf("%d", &size_y)
+
+	for i := 0; i < size_y; i++ {
+		if i%2 == 0 {
+			for i := 0; i < size_x; i++ {
+				if i%2 == 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+				if i == size_x-1 {
+					fmt.Println("")
+				}
+			}
+		} else {
+			for i := 0; i < size_x; i++ {
+				if i%2 == 0 {
+					fmt.Print(" ")
+				} else {
+					fmt.Print("#")
+				}
+				if i == size_x-1 {
+					fmt.Println("")
+				}
+			}
+		}
+	}
 }
