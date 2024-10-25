5ac429418245d24a6dbd55d4b7a0c360c790a86e
PapaDjo2000 <maksimdadychyn@gmail.com>
2024-10-25 20:07:34 +0300

    hw03_chessboard completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -2,38 +2,35 @@ package main
 
 import "fmt"
 
+func X(index int, sizeX int) {
+	if index%2 == 0 {
+		fmt.Print(" ")
+	} else {
+		fmt.Print("#")
+	}
+	if index == sizeX-1 {
+		fmt.Println("")
+	}
+}
+
 func main() {
-	var size_x int
-	var size_y int
+	var sizeX int
+	var sizeY int
 
 	fmt.Println("Введите Х")
-	fmt.Scanf("%d", &size_x)
+	fmt.Scanf("%d", &sizeX)
 
 	fmt.Println("Введите У")
-	fmt.Scanf("%d", &size_y)
+	fmt.Scanf("%d", &sizeY)
 
-	for i := 0; i < size_y; i++ {
+	for i := 0; i < sizeY; i++ {
 		if i%2 == 0 {
-			for i := 0; i < size_x; i++ {
-				if i%2 == 0 {
-					fmt.Print("#")
-				} else {
-					fmt.Print(" ")
-				}
-				if i == size_x-1 {
-					fmt.Println("")
-				}
+			for index := 0; index < sizeX; index++ {
+				X(index, sizeX)
 			}
 		} else {
-			for i := 0; i < size_x; i++ {
-				if i%2 == 0 {
-					fmt.Print(" ")
-				} else {
-					fmt.Print("#")
-				}
-				if i == size_x-1 {
-					fmt.Println("")
-				}
+			for index := 1; index < sizeX; index++ {
+				X(index, sizeX)
 			}
 		}
 	}
