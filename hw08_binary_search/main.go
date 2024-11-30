5f5baf12091575cec0e7562039c4f345546dc00e
PapaDjo2000 <maksimdadychyn@gmail.com>
2024-11-30 18:57:56 +0300

    hw08_binary_search completed

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	search "github.com/PapaDjo2000/hw08_binary_search/pkg"
+)
+
 func main() {
-	// Place your code here.
+	mass := []int{1, 2, 4, 5, 7, 8}
+	target := 5
+
+	result := search.BinarySearch(target, mass)
+
+	if result == -1 {
+		fmt.Println("обьект не найден")
+	} else {
+		fmt.Printf("обьект %d находиться по индексу %d\n", target, result)
+	}
 }
