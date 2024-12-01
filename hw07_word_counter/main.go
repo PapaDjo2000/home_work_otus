c9269ebba93c3ba6147685c929615d95691d7ce7
PapaDjo2000 <maksimdadychyn@gmail.com>
2024-12-01 16:54:58 +0300

    hw07_word_counter completed

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func FixString(str string) []string {
+	str = strings.ToLower(str)
+	sli := []string{",", ".", "!", "?", "1", "2", "3", "4"}
+
+	str = strings.Join(strings.Fields(str), " ")
+
+	for key := range sli {
+		str = strings.ReplaceAll(str, sli[key], "")
+	}
+
+	s := strings.Split(str, " ")
+	return s
+}
+
+func CountWord(str string) map[string]int {
+	sli := FixString(str)
+
+	mapa := make(map[string]int)
+
+	for _, v := range sli {
+		mapa[v]++
+	}
+
+	return mapa
+}
+
 func main() {
-	// Place your code here.
+	str := CountWord("Строка,. которая213 повторяет строку ?которая строка!")
+	fmt.Println(str)
+	fmt.Println(str["строка"])
 }
