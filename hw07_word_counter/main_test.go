fed27efa8a780676c31d817106e858664b13f04e
PapaDjo2000 <maksimdadychyn@gmail.com>
2024-12-02 22:37:59 +0300

    fix test

--- hw07_word_counter/main_test.go
+++ hw07_word_counter/main_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestMain_FixString(t *testing.T) {
-	str := "!строка строки структуры12 строки."
+	str := "!строка строки: ;структуры строки."
 	want := []string{"строка", "строки", "структуры", "строки"}
 	assert.Equal(t, want, FixString(str))
 }
 
 func TestMain_CountWord(t *testing.T) {
-	str := "!строка строки структуры12 строки."
+	str := "!строка строки: ;структуры строки."
 	want := map[string]int{"строка": 1, "строки": 2, "структуры": 1}
 	assert.Equal(t, want, CountWord(str))
 }
