5cfb74db81623794dd645172c60edc9128ce7cf3
PapaDjo2000 <maksimdadychyn@gmail.com>
2025-01-06 21:29:50 +0300

    hw11_worker_pool completed

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var count int
+
+func work(a int, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		mu.Lock()
+		count++
+		mu.Unlock()
+	}
+	fmt.Printf("gorutine %d the end \n", a)
+}
+
 func main() {
-	// Place your code here.
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go work(i, &wg, &mu)
+	}
+	wg.Wait()
+	fmt.Println(count)
 }
